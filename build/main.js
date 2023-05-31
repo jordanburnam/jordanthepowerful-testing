9ede3be73232d0d41a487ac16b3969671ebc04f2
Jordan Burnam <me@jordanburnam.com>
2023-05-31 12:04:49 -0400

    Init Commit

---
+++ build/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jordanthepowerful_sdk_1 = require("jordanthepowerful-sdk");
+let a = new jordanthepowerful_sdk_1.Jordan("Wodnerful");
+a.sayit();
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { Jordan } from "jordanthepowerful-sdk"
+
+let a:Jordan = new Jordan("Wodnerful");
+a.sayit();
